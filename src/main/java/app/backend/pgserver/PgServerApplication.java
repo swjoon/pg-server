4703acd647fe730b3d14e688ad5680dbd4223587
swjoon <jetkid1446@gmail.com>
2026-04-29 18:39:24 +0900

    initial setting

---
+++ src/main/java/app/backend/pgserver/PgServerApplication.java
@@ -0,0 +1,13 @@
+package app.backend.pgserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/backend/pgserver/PgServerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.backend.pgserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
